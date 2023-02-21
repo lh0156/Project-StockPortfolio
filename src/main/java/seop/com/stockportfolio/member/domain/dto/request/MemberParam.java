e859e5c44ca7c03244881a777303a429e53f6b8a
YunSeop <62834176+lh0156@users.noreply.github.com>
2023-02-21 12:52:08 +0900

    fetch: memberPara 생성

---
+++ src/main/java/seop/com/stockportfolio/member/domain/dto/request/MemberParam.java
@@ -0,0 +1,14 @@
+package seop.com.stockportfolio.member.domain.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class MemberParam {
+
+    String id;
+    String name;
+    String password;
+    String passwordOk;
+    String phoneNumber;
+
+}
